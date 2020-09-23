cddcffb58beab0a377d78226c0803263ab38ac59
Greqan <greqan@gmail.com>
2020-09-23 19:59:40 +0200

    Script v.1.1

--- gog-scrapper.py
+++ gog-scrapper.py
@@ -8,43 +8,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from retrying import retry
 
 
-def wishlist_exist(browser):
-    html = browser.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    return False if soup.body.find_all('div', class_="empty__message") else True
+def retry_if_timeout_error(exception):
+    """Return True if we should retry (in this case when it's an IOError), False otherwise"""
+    return isinstance(exception, TimeoutError)
+
+
+class BrowserContext:
+    browser = webdriver.Chrome()
+
+    def __init__(self, username):
+        self.browser.set_page_load_timeout(15)
+        try:
+            url = "https://www.gog.com/u/" + username + "/wishlist"
+            self.browser.get(url)
+        except TimeoutError:
+            raise
+
+    def wishlist_exist(self):
+        html = self.browser.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        return False if soup.body.find_all('div', class_="empty__message") else True
+
+    def click_right_arrow(self, css_selector_right_arrow):
+        arrow = self.browser.find_element(By.CSS_SELECTOR, css_selector_right_arrow)
+        self.browser.execute_script("arguments[0].click();", arrow)
+
+    @retry(retry_on_exception=retry_if_timeout_error, stop_max_attempt_number=3)
+    def scrolling_down_totally(self, username, tries=3):
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+            WebDriverWait(self.browser, 15).until(
+                EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[class="footer-microservice-secondary__legal"]'))
+            )
+
+
+def next_price(soup):
+    for price in soup.body.find_all('span', class_="product-state__price"):
+        yield price.text
 
 
 def retrieve_single_table(soup):
-    records_dict_tmp = {}
-    price_list_tmp = []
-    for price in soup.body.find_all('span', class_="product-state__price"):
-        price_list_tmp.append(price.text)
-    price_list_tmp.reverse()
-    for record in soup.body.find_all('span', class_="product-title__text"):
-        records_dict_tmp[record.text] = price_list_tmp.pop()
-    return records_dict_tmp
-
-
-def click_right_arrow(browser, css_selector_right_arrow):
-    arrow = browser.find_element(By.CSS_SELECTOR, css_selector_right_arrow)
-    browser.execute_script("arguments[0].click();", arrow)
-
-
-def scrolling_down_totally(browser, username, tries=3):
-    try:
-        browser.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-        WebDriverWait(browser, 15).until(
-            EC.visibility_of_element_located((By.CSS_SELECTOR, 'div[class="footer-microservice-secondary__legal"]'))
-        )
-    except TimeoutError:
-        tries = tries-1
-        if tries > 0:
-            scrolling_down_totally(browser, username)
-        else:
-            print("It was impossible to scroll down.")
-            raise
+    records = soup.body.find_all('span', class_="product-title__text")
+    records_dict = {records.pop(0).text: price for price in next_price(soup)}
+    print(records_dict)
+    return records_dict
 
 
 def append_to_json(username, single_site_table):
@@ -52,38 +61,40 @@ def append_to_json(username, single_site_table):
         json.dump(single_site_table, fresult, indent=4)
 
 
+def get_soup_from_source(browserContext):
+        html = browserContext.browser.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        return soup
+
+
+def handle_more_than_one_page(browserContext, pages_total, username):
+    css_selector_right_arrow = "span[hook-test='nextListPage']"
+    for i in range(0, pages_total):
+        soup = get_soup_from_source(browserContext)
+        WebDriverWait(browserContext.browser, 15).until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector_right_arrow))
+        )
+        single_page_table = retrieve_single_table(soup)
+        append_to_json(username, single_page_table)
+        browserContext.click_right_arrow(css_selector_right_arrow)
+        # ensuring that html after parsing has changed after arrow click
+        while single_page_table == retrieve_single_table(soup) and i < pages_total - 1:
+            soup = get_soup_from_source(browserContext)
+
+
 def retrieve_wishlist(username):
     with open(username + "_wishlist.json", "w") as fresult:
         fresult.write("")
-    css_selector_right_arrow = "span[hook-test='nextListPage']"
-    browser = webdriver.Chrome()
-    browser.set_page_load_timeout(15)
-    try:
-        url = "https://www.gog.com/u/" + username + "/wishlist"
-        browser.get(url)
-    except TimeoutError:
-        raise
-    html = browser.page_source
-    soup = BeautifulSoup(html, "html.parser")
-    if wishlist_exist(browser):
-        scrolling_down_totally(browser, username)
+    browserContext = BrowserContext(username)
+    get_soup_from_source(browserContext)
+    if browserContext.wishlist_exist():
+        browserContext.scrolling_down_totally(username)
         #checking if there is more than one page
-        pages_navi = soup.body.find('span', class_="list-navigation__pagin")
+        pages_navi = get_soup_from_source(browserContext).body.find('span', class_="list-navigation__pagin")
+        soup = get_soup_from_source(browserContext)
         if pages_navi is not None:
             pages_total = int(soup.body.find_all('span', class_="pagin__total").pop().text)
-            for i in range(0, pages_total):
-                html = browser.page_source
-                soup = BeautifulSoup(html, "html.parser")
-                WebDriverWait(browser, 15).until(
-                    EC.visibility_of_element_located((By.CSS_SELECTOR, css_selector_right_arrow))
-                )
-                single_page_table = retrieve_single_table(soup)
-                append_to_json(username, single_page_table)
-                click_right_arrow(browser, css_selector_right_arrow)
-                #ensuring that html after parsing has changed after arrow click
-                while single_page_table == retrieve_single_table(soup) and i < pages_total-1:
-                    html = browser.page_source
-                    soup = BeautifulSoup(html, "html.parser")
+            handle_more_than_one_page(browserContext, pages_total, username)
         else:
             single_page_table = retrieve_single_table(soup)
             append_to_json(username, single_page_table)
@@ -98,7 +109,7 @@ def retrieve_image_url(username):
         print("Username does not exist.")
         raise
     with open(username+".html", encoding="utf-8") as html_f:
-        
+
         soup_site = BeautifulSoup(html_f, "html.parser")
         links = []
         for tag in soup_site.body.find_all('a',  class_="user-status__avatar-link"):
